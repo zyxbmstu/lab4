6964c1ff51d1d810f33d04acf70759a3fa787a6a
zyxbmstu <zaxarler@mail.ru>
2021-12-10 13:24:32 +0300

    Scripted auto-commit on change (2021-12-10 13:24:32) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/Server.java
+++ src/main/java/bmstu/iu9/lab4/Server.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        ActorSystem system = ActorSystem.create("routes")
+        ActorSystem system = ActorSystem.create("routes");
     }
 
 }
