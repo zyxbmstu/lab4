7e42222288183f12474f4dd864cec37352cc7209
zyxbmstu <zaxarler@mail.ru>
2021-12-10 17:00:53 +0300

    Scripted auto-commit on change (2021-12-10 17:00:53) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/ExecuteActor.java
+++ src/main/java/bmstu/iu9/lab4/ExecuteActor.java
@@ -2,6 +2,7 @@ package bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import bmstu.iu9.lab4.message.StorageMessage;
 import bmstu.iu9.lab4.message.TestMessage;
 
 public class ExecuteActor extends AbstractActor {
@@ -9,7 +10,7 @@ public class ExecuteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(TestMessage.class, msg -> sender().tell())
+                .match(TestMessage.class, msg -> sender().tell(new StorageMessage(msg.getPackageId()),))
     }
 
 }
