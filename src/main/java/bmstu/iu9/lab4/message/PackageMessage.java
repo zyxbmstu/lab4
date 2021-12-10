20e3ff863d4b343ca65d5041f8a92fd8520dc075
zyxbmstu <zaxarler@mail.ru>
2021-12-10 17:39:56 +0300

    Scripted auto-commit on change (2021-12-10 17:39:56) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/message/PackageMessage.java
+++ src/main/java/bmstu/iu9/lab4/message/PackageMessage.java
@@ -1,7 +1,15 @@
 package bmstu.iu9.lab4.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class PackageMessage {
 
-    
+    public PackageMessage(@JsonProperty() int packageId,
+                          @JsonProperty() ArrayList<Test> tests) {
+        this.packageId = packageId;
+        this.tests = tests;
+    }
 
 }
