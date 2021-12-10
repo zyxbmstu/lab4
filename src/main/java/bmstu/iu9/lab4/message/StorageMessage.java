8713797cbc66968101b855541c53f09b5bfa7826
zyxbmstu <zaxarler@mail.ru>
2021-12-10 16:44:13 +0300

    Scripted auto-commit on change (2021-12-10 16:44:13) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/message/StorageMessage.java
+++ src/main/java/bmstu/iu9/lab4/message/StorageMessage.java
@@ -1,4 +1,29 @@
 package bmstu.iu9.lab4.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class StorageMessage {
+
+    private int packageId;
+    private ArrayList<Test> tests;
+
+    private final String PACKAGE_ID = "packageId";
+    private final String TESTS = "test";
+
+    public StorageMessage(@JsonProperty(PACKAGE_ID) int packageId,
+                          @JsonProperty(TESTS)ArrayList<Test> tests) {
+        this.packageId = packageId;
+        this.tests = tests;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public ArrayList<Test> getTests() {
+        return tests;
+    }
+
 }
