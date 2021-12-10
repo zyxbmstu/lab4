59ba5b5836fdbfda1733ba94516b29eb80eb0cf4
zyxbmstu <zaxarler@mail.ru>
2021-12-10 17:44:48 +0300

    Scripted auto-commit on change (2021-12-10 17:44:48) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/message/PackageMessage.java
+++ src/main/java/bmstu/iu9/lab4/message/PackageMessage.java
@@ -6,10 +6,40 @@ import java.util.ArrayList;
 
 public class PackageMessage {
 
-    public PackageMessage(@JsonProperty() int packageId,
-                          @JsonProperty() ArrayList<Test> tests) {
+    private final String PACKAGE_ID = "packageId";
+    private final String SCRIPT = "script";
+    private final String FUNCTION_NAME = "functionName";
+    private final String TESTS = "tests";
+
+    private int packageId;
+    private String script;
+    private String functionName;
+    private ArrayList<Test> tests;
+
+    public PackageMessage(@JsonProperty(PACKAGE_ID) int packageId,
+                          @JsonProperty(SCRIPT) String script,
+                          @JsonProperty(FUNCTION_NAME) String functionName,
+                          @JsonProperty(TESTS) ArrayList<Test> tests) {
         this.packageId = packageId;
         this.tests = tests;
+        this.functionName = functionName;
+        this.script = script;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public String getFunctionName() {
+        return getFunctionName();
+    }
+
+    public ArrayList<Test> getTests() {
+        return tests;
     }
 
 }
