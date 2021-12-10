36a0e3ccfad01ff6f401f0bce7fcf5fb99c22548
zyxbmstu <zaxarler@mail.ru>
2021-12-10 16:07:47 +0300

    Scripted auto-commit on change (2021-12-10 16:07:47) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/StorageActor.java
+++ src/main/java/bmstu/iu9/lab4/StorageActor.java
@@ -1,12 +1,14 @@
 package bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class StorageActor extends AbstractActor {
 
     @Override
     public Receive createRecieve() {
-        return 
+        return ReceiveBuilder.create()
+                .match()
     }
 
 }
