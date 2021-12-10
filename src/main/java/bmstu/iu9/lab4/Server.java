1c9a5faba76f3546e3ff3d7ae218f117727b8d8a
zyxbmstu <zaxarler@mail.ru>
2021-12-10 18:39:38 +0300

    Scripted auto-commit on change (2021-12-10 18:39:38) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/Server.java
+++ src/main/java/bmstu/iu9/lab4/Server.java
@@ -10,7 +10,7 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.scaladsl.server.Route;
+import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
