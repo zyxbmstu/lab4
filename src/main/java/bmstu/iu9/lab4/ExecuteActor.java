d4084fd9a37b2d423633094333176f89578760a7
zyxbmstu <zaxarler@mail.ru>
2021-12-10 16:58:24 +0300

    Scripted auto-commit on change (2021-12-10 16:58:24) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/ExecuteActor.java
+++ src/main/java/bmstu/iu9/lab4/ExecuteActor.java
@@ -1,4 +1,12 @@
 package bmstu.iu9.lab4;
 
-public class ExecuteActor {
+import akka.actor.AbstractActor;
+
+public class ExecuteActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        
+    }
+
 }
