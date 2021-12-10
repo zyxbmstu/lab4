34fb1f3bb1d93fd9e81d30d72699cf0ce04c758a
zyxbmstu <zaxarler@mail.ru>
2021-12-10 17:52:01 +0300

    Scripted auto-commit on change (2021-12-10 17:52:01) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/Server.java
+++ src/main/java/bmstu/iu9/lab4/Server.java
@@ -1,8 +1,13 @@
 package bmstu.iu9.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
+import akka.http.scaladsl.model.HttpRequest;
+import akka.http.scaladsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 
@@ -19,6 +24,12 @@ public class Server {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
         Server app = new Server(system);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.create
+    }
+
+    private Route createRoute() {
+        return route
     }
 
 }
