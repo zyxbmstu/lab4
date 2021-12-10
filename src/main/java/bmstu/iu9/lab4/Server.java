27aa7632b4e53b5348bd682ce67a367c2b8bd5bd
zyxbmstu <zaxarler@mail.ru>
2021-12-10 13:29:14 +0300

    Scripted auto-commit on change (2021-12-10 13:29:14) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/Server.java
+++ src/main/java/bmstu/iu9/lab4/Server.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Server {
 
     private Server(final ActorSystem system) {
-        
+
     }
 
     public static void main(String[] args) throws IOException {
