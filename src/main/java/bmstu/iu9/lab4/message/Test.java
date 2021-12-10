abf46f7964e9de7dc8e631aa6c059c2cce597610
zyxbmstu <zaxarler@mail.ru>
2021-12-10 16:25:44 +0300

    Scripted auto-commit on change (2021-12-10 16:25:44) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/message/Test.java
+++ src/main/java/bmstu/iu9/lab4/message/Test.java
@@ -1,7 +1,18 @@
 package bmstu.iu9.lab4.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
 
-    public Test()
+    private final String TEST_NAME = "testName";
+    private final String PARAMET = "testName";
+    private final String TEST_NAME = "testName";
+
+    @JsonProperty(TEST_NAME)
+    private String testName;
+
+
+    public Test(@JsonProperty(TEST_NAME) String testNAme,
+                )
 
 }
