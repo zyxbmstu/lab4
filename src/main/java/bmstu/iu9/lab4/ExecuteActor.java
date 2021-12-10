043a8cdf24bac2ccb64c35cebfe1834b8fd505ce
zyxbmstu <zaxarler@mail.ru>
2021-12-10 20:38:58 +0300

    Scripted auto-commit on change (2021-12-10 20:38:58) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/ExecuteActor.java
+++ src/main/java/bmstu/iu9/lab4/ExecuteActor.java
@@ -7,9 +7,11 @@ import bmstu.iu9.lab4.message.StorageMessage;
 import bmstu.iu9.lab4.message.Test;
 import bmstu.iu9.lab4.message.TestMessage;
 
-import javax.script.*;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ExecuteActor extends AbstractActor {
 
@@ -28,24 +30,12 @@ public class ExecuteActor extends AbstractActor {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(LANGUAGE);
 
         System.out.println(msg.getScript());
-        //System.out.println(msg.getScript().getClass());
+        System.out.println(msg.getScript().getClass());
 
-        ScriptEngineManager manager = new ScriptEngineManager();
-        List<ScriptEngineFactory> factories = manager.getEngineFactories();
-        for (ScriptEngineFactory factory : factories) {
-            System.out.println(factory.getEngineName());
-            System.out.println(factory.getEngineVersion());
-            System.out.println(factory.getLanguageName());
-            System.out.println(factory.getLanguageVersion());
-            System.out.println(factory.getExtensions());
-            System.out.println(factory.getMimeTypes());
-            System.out.println(factory.getNames());
-        }
-
-        /*engine.eval(msg.getScript());
+        engine.eval(msg.getScript());
         Invocable invocable = (Invocable) engine;
         String testResult = invocable.invokeFunction(msg.getFunctionName(), msg.getTest().getParams().toArray()).toString();
-*/
+
         Test test = new Test(msg.getTest().getTestName(),
                 msg.getTest().getExpectedResult(),
                 msg.getTest().getParams(),
