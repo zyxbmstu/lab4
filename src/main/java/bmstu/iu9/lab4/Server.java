d866c4bef52853d4eaeafd454d6768a172169e2c
zyxbmstu <zaxarler@mail.ru>
2021-12-10 13:24:30 +0300

    Scripted auto-commit on change (2021-12-10 13:24:30) by gitwatch.sh

--- src/main/java/bmstu/iu9/lab4/Server.java
+++ src/main/java/bmstu/iu9/lab4/Server.java
@@ -1,4 +1,13 @@
 package bmstu.iu9.lab4;
 
+import akka.actor.ActorSystem;
+
+import java.io.IOException;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("routes")
+    }
+
 }
